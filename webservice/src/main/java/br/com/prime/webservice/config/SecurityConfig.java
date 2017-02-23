10c789b21553370095294c2dc0124ec4afe0a5b7
= <=>
2017-02-23 17:22:52 -0300

    iniciando o spring security

---
+++ webservice/src/main/java/br/com/prime/webservice/config/SecurityConfig.java
@@ -0,0 +1,7 @@
+package br.com.prime.webservice.config;
+
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+
+}
---
+++ webservice/src/main/java/br/com/prime/webservice/config/SecurityInitializer.java
@@ -0,0 +1,7 @@
+package br.com.prime.webservice.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityInitializer extends AbstractSecurityWebApplicationInitializer{
+
+}
