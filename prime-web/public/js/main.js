9a033fe75833102ace36e23710cddeb3d7dde452
= <=>
2017-03-31 17:28:43 -0300

    adicionando tratativa inicial de rotas com ui-route

--- prime-web/public/js/main.js
+++ prime-web/public/js/main.js
@@ -1,8 +1,7 @@
-angular.module('prime', ['nrRoute']).config(function($routeProvider){
+var login = angular.module('prime', ['ui.router']);
 
-    $routeProvider.when('/login', {
-      templateUrl: 'partials/login/login.html',
-      controller: 'LoginController'
-    });
-
+login.config(function($stateProvider, $urlRouterProvider){
+  $urlRouterProvider.otherwise('/404');
+  $stateProvider.state('login', {url: '/login', templateUrl : '../partials/login/login.html', controller : 'LoginController'});
+  $stateProvider.state('404', {url: '/404', templateUrl : '../404.html'} );
 });
