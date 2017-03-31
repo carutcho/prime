3a46918858ae588dc6f87ad9289fef01d9d05ff1
= <=>
2017-03-31 17:38:23 -0300

    ajustando nome de pastas

--- prime-web/public/js/main.js
+++ prime-web/public/js/main.js
@@ -2,6 +2,6 @@ var login = angular.module('prime', ['ui.router']);
 
 login.config(function($stateProvider, $urlRouterProvider){
   $urlRouterProvider.otherwise('/404');
-  $stateProvider.state('login', {url: '/login', templateUrl : '../partials/login/login.html', controller : 'LoginController'});
+  $stateProvider.state('login', {url: '/login', templateUrl : '../templates/login/login.html', controller : 'LoginController'});
   $stateProvider.state('404', {url: '/404', templateUrl : '../404.html'} );
 });
